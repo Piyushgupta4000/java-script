de1c9bd3b99865e25ea176d1c5bb031bc995f51a
Piyushgupta4000 <piyushgupta57579@gmail.com>
2025-04-22 18:05:02 +0000

    data types

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,28 @@
+"useStrict";//treat all js code AS NEW version
+//alert(3+3);//we are using node js not browser,java script avoid extra spaces
+
+// alert("bhut garmi hai aj phir v mai java script sikhunga");
+
+/*console.log(3+
+    3); code readablity should be high
+    */
+let name = "piyush"
+let age = 15
+let isLoggedIn = false
+let state
+
+//number=2 to power 53
+// bigint
+//string = ""
+// boolean = true,false
+// null = stand alone value
+// undefined= variable decleared but valued not assign
+// symbol unique
+
+// object
+console.log(typeof "piyush");
+console.log(typeof ("piyush"));// both syntax write
+
+console.log(typeof age);
+console.log(typeof null);// null object
+console.log(typeof undefined);// undefined
