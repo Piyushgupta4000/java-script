d4fe0e9349e4cd0515c864bf84763ffadee30e09
Piyushgupta4000 <piyushgupta57579@gmail.com>
2025-04-24 16:43:43 +0000

    strings learn and with his prototype

---
+++ 01_basic/05_strings.js
@@ -0,0 +1,25 @@
+const name ="piyush";
+const repoCount= 11;
+
+// console.log(name+repoCount+"value"); old days use 
+
+// console.log(`my name is ${name} and my repo count is ${11}`);
+
+//string declare
+
+const name2 = new String("piyush");
+// console.log(name2);
+// console.log(name2[1]);
+// console.log(name2.__proto__);
+// console.log(name2.length);
+// console.log(name2.toUpperCase());
+// console.log(name2.toLocaleLowerCase());
+// console.log(name2.fontsize());
+// console.log(name2.charAt(2));
+// console.log(name2.indexOf("y"));
+
+// const newString =name2.substring(0,4);// 4 index position not include
+// console.log(newString);
+
+const anotherString =name2.slice(-7,4);
+console.log(anotherString);
