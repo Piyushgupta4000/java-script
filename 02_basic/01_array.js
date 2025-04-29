fb9258a873a379c109c906d3ba167c3223f63e47
Piyush Gupta <piyushgupta57579@gmail.com>
2025-04-29 06:01:53 +0000

    array basic

---
+++ 02_basic/01_array.js
@@ -0,0 +1,46 @@
+//array declare
+// const myArr = [0,1,2,3,4,5,true,"piyush"];
+const myArr = [0,1,2,3,4,5];
+const mySkill =["html","css","js"];
+const myArr2 =new Array (1,2,3);
+// console.log(myArr2[0]);
+
+
+// array Method
+// myArr.push(6);
+// myArr.push(7,8,9);
+// myArr.pop();
+// myArr.unshift(10);
+// myArr.shift();
+
+
+
+
+// console.log(myArr.includes(5));
+// console.log(myArr.indexOf(3));
+
+// console.log(myArr.indexOf(20));
+// console.log(myArr.indexOf(+5));
+// console.log(myArr.indexOf(-10));
+// console.log(myArr.indexOf(-3));
+
+// const newArr =myArr.join();
+// console.log(myArr);
+// console.log(typeof(myArr));
+// console.log(newArr);
+// console.log(typeof(newArr));
+
+
+// slice or splice
+
+console.log("A " ,myArr);
+
+const myN1 = myArr.slice(0,3);
+console.log(myN1);
+
+console.log("B =",myArr);
+
+const myN2 =myArr.splice(0,3);
+console.log(myN2);
+
+console.log("C = ",myArr);
