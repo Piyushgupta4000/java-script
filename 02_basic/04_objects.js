be2307f07a1517c8e5638a1c7b5a5babc4ce5a24
Piyush Gupta <piyushgupta57579@gmail.com>
2025-05-01 05:18:49 +0000

    jason

--- 02_basic/04_objects.js
+++ 02_basic/04_objects.js
@@ -38,5 +38,41 @@ const obj2 = {
 // console.log(obj1 , obj2);
 
 
-const obj3 = Object.assign(obj1,obj2);
-console.log(obj3);
+// const obj3 = Object.assign({},obj1,obj2);
+const obj3 = {...obj1,...obj2};
+// console.log(obj3);
+
+// console.log(tinderUser);
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+// console.log(tinderUser.hasOwnProperty(`isLoggedIn`));
+
+const course = {
+    courseNAme: "Js in hindi",
+    coursePrice: 999,
+    cousreInstructor: "hitesh sir"
+
+}
+
+// course.cousreInstructor;
+
+const {cousreInstructor:instructor} =course;
+
+// console.log(cousreInstructor);
+// console.log(instructor);
+
+// Api basic
+
+// {
+//     "name":"piyush",
+//     "couurseNAme": "js in hindi ",
+//     "coursePrice": "happy"
+// }
+
+
+[
+    {},
+    {},
+    {},
+]
