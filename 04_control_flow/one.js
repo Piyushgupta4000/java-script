d362c22bf5fca9b732a102357817b6411588548e
Piyush Gupta <piyushgupta57579@gmail.com>
2025-05-05 05:35:09 +0000

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,52 @@
+// if
+// const isUserLoggedIn = true;
+// const temperature = 50;
+// if( temperature === 51){
+// console.log("temperatur less the 50");
+// } else{
+//     console.log("temperatue greter then 50 ");
+// }
+
+// scope story
+
+const score = 200;
+
+// if (score>100){
+//     let power = "fly";
+//     console.log(`user power:${power}`);
+// }
+// // console.log(`user power:${power}`);
+
+
+const balance = 5000;
+// if(balance>1000) console.log("pass test "),console.log("pass test2"); // bad practise 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// compresion operatort
+// ==     // Equal to (loose comparison)
+// ===    // Equal to (strict comparison with check same data type)
+// !=     // Not equal to
+// !==    // Not equal (strict)
+// >      // Greater than
+// <      // Less than
+// >=     // Greater than or equal to
+// <=     // Less than or equal to
+
+
