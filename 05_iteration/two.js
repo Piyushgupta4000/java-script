b64afb0cc84fe89b43a4583bd592e2009ebe8337
Piyush Gupta <piyushgupta57579@gmail.com>
2025-05-05 15:35:43 +0000

    while do while loop

---
+++ 05_iteration/two.js
@@ -0,0 +1,20 @@
+let index = 0;
+while (index<=10) {
+    // console.log(index);
+    index = index+3;
+   
+}
+
+let myarray = ["A","B","C"]
+let arr = 0
+while(arr<myarray.length){
+// console.log(`value of ${myarray[arr]}`)
+arr = arr+1;
+}
+
+
+let score = 0
+do {
+    console.log(`score is ${score}`)
+    score++
+} while (score<=20);
