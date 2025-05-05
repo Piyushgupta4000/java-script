dd8924889d10703a098b24a91e9e86508f80db2d
Piyush Gupta <piyushgupta57579@gmail.com>
2025-05-05 15:16:49 +0000

    continue and break keyword

--- 05_iteration/one.js
+++ 05_iteration/one.js
@@ -15,3 +15,22 @@ for (let i = 0; i <= 10; i++) {
         
     }
 }
+let myArray = ["a" ,"b", "c"]
+// console.log(myArray.length);
+for (let index = 0; index < myArray .length; index++) {
+    const element = myArray [index];
+    // console.log(element)
+    
+}
+
+
+
+// break and continue keyword
+
+for(i=0; i<=20; i++){
+    if(i==5){
+        console.log("Detected 5");
+        break
+    }
+    console.log(i);
+}
